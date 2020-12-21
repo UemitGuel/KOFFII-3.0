8c3195a0b8cc6518d862fab434bd32ef474e2dc5
Ümit <47325422+quechupiOS@users.noreply.github.com>
2020-12-21 22:55:10 +0100

    less text for sabor

---
+++ KOFFII CGN 2.0/CafeView/FilterSwitch.swift
@@ -0,0 +1,20 @@
+//
+//  FilterSwitch.swift
+//  KOFFII CGN 2.0
+//
+//  Created by Gül, Ümit on 21.12.20.
+//
+
+import SwiftUI
+
+struct FilterSwitch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterSwitch_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterSwitch()
+    }
+}
